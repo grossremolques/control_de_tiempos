ef4706fba8d97529e7fa00b83d9765ebc587414f
Kathe <kathejudith@gmail.com>
2025-05-15 10:36:31 -0300

    deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import { useAuth } from "../context/AuthContext";
 export default function Home() {
   const { user } = useAuth();
   return (
-    <>
-      <h1 className="text-xl">¡Hola 👋, {user.nombre}!</h1>
-    </>
+    <div className="flex justify-center items-center mt-20">
+      <h1 className="text-2xl">¡Hola 👋, {user.nombre}!</h1>
+    </div>
   );
 }
